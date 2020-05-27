322891e4ccab687470e1e4658742885c01e3417a
Linh Dang <linh.danghong@gmail.com>
2020-05-27 23:31:52 +0100

    set base agents

---
+++ src/main/java/mams/Plan.java
@@ -0,0 +1,26 @@
+package mams;
+import java.util.Date;
+
+import mams.utils.Identifier;
+
+/**
+ * A planning session with User
+ * User express interest on flights from Date - to Date in a location, and accomodation type (1 bed, 2 bed, full appartment)
+ */
+public class Plan {
+
+    @Identifier
+    public String name;
+
+    // Destination for toursim
+    public String destination;
+    // Location when the flight start
+    public String location;
+    // Flight and accomodation start Date, assume both are the same
+    public Date from;
+     // Flight and accomodation start Date, assume both are the same
+    public Date to;
+    // accomodationtype ONE_BED, TWO_BEND, FULL_HOUSE, should be an enum but we can optimize later
+    public String accomodationType;
+    
+}
---
+++ src/main/java/mams/Test.java
@@ -0,0 +1,10 @@
+package mams;
+
+import mams.utils.Identifier;
+
+public class Test {
+    public String message;
+
+    @Identifier
+    public String name;
+}
